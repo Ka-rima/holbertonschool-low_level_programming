e0083045bdbcf7556990d2c1f6b2864c79214e45
Ka-rima <touileb.karima@icloud.com>
2025-02-17 10:00:50 +0000

    Correction of thetrailing whitespace and description

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+/**
+* main - Entry main
+*
+* Return: Always 0
+*/
+
 int main(void)
-{puts("\""Programming is like building a multilingual puzzle");
-	return 0;}
+{
+	puts("\""Programming is like building a multilingual puzzle");
+
+	return (0);
+}
+
