98eda9844c1e0a2b28b2c9ae9378cb1f217faab5
Ka-rima <touileb.karima@icloud.com>
2025-02-17 14:37:12 +0000

    I added an extra lign to include the long int 8bytes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,11 +14,12 @@ int main(void)
 	long long longlongType;
 	float floatType;
 
-	printf("Size of a char: 1 byte(s)\n");
-	printf("Size of an int: 4 byte(s)\n");
-	printf("Size of a long int: 4 byte(s)\n");
-	printf("Size of a long long int: 8 byte(s)\n");
-	printf("Size of a float: 4 byte(s)\n");
+	printf("Size of a char: 1 byte(s)\n", sizeof(charType));
+	printf("Size of an int: 4 byte(s)\n", sizeof(intType));
+	printf("Size of a long int: 4 byte(s)\n", sizeof(longintType));
+	printf("Size of a long int: 8 byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: 8 byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: 4 byte(s)\n", sizeof(floatType));
 
 	return (0);
 }
