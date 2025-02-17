ced08de07c2c212eda27f848d0f9e2a35636830e
Ka-rima <touileb.karima@icloud.com>
2025-02-17 10:10:16 +0000

    My script has been deleted so I rewrite it

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-/**
-* main - Entry main
-*
-* Return: Always 0
-*/
 
+/**
+ * main - Entry main
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	puts("\""Programming is like building a multilingual puzzle");
+	puts("\"Programing is like building a multilingual puzzle");
 
 	return (0);
 }
-
