30edc0be7ad9865389afd75aa5e3ceaab372a988
Ka-rima <touileb.karima@icloud.com>
2025-02-18 08:41:48 +0000

    I removed some spaces

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-
 /**
-*main - Entry main
+* main - Entry point
 *
-*Return: Always 0
+* Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	char charType;
-	int intType;
-	long longType;
-	long long longlongType;
-	float floatType;
+	char CharType;
+	int IntType;
+	long LongType;
+	long LonglongType;
+	float FloatType;
 
-	printf("Size of a char: \n%ld byte(s)", sizeof(char));
-	printf("Size of an int: \n%ld byte(s)", sizeof(int));
-	printf("Size of a long int:\n%ld byte(s)", sizeof(long int));
-	printf("Size of a long long int: \n%ld byte(s)", sizeof(long long int));
-	printf("Size of a float: \n%ld byte(s)", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int:%lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
 }
