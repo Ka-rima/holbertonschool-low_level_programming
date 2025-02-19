04d4ac958032503773563e2dbafd78b447626e06
Ka-rima <touileb.karima@icloud.com>
2025-02-19 09:29:55 +0000

    I removed some unnecessary spaces

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	/** declaration of a variable */
+	/** declaration of variables */
 	int n;
 	int k;
 
@@ -18,18 +18,14 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	k = n % 10;
 
-	if(k > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, k);
-	}
-	if else(k = 0)
-	{
-		printf("Last digit of %d is %d and is zero\n", n, k);
-	}
-	else(k < 6)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, k);
-	}
+	if (k > 5)
+	printf("Last digit of %d is %d and is greater than 5\n", n, k);
+
+	if (k == 0)
+	printf("Last digit of %d is %d and is 0\n", n, k);
+	
+	if (k < 6)
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, k);
 	return (0);
 }
 
