fa744cf3bd87691c427e0a83675218d07f27d3f1
Ka-rima <touileb.karima@icloud.com>
2025-02-19 09:25:53 +0000

    I removed some spaces before the brackets

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,17 +18,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	k = n % 10;
 
-	if (k > 5)
+	if(k > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, k);
+		printf("Last digit of %d is %d and is greater than 5\n", n, k);
 	}
-	if (k == 0)
+	if else(k = 0)
 	{
-	printf("Last digit of %d is %d and is zero\n", n, k);
+		printf("Last digit of %d is %d and is zero\n", n, k);
 	}
-	if (k > 6)
+	else(k < 6)
 	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, k);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, k);
 	}
 	return (0);
 }
