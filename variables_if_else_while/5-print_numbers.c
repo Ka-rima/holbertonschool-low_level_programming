9b6c9161291931d508350a790d895a60177eefe2
Ka-rima <touileb.karima@icloud.com>
2025-02-19 15:29:47 +0000

    I corrected my code according to Betty style

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	/** declaration of variables */
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
+
