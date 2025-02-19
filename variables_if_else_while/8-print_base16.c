0e4a2d64597798695b1d1e683096fe732dec1777
Ka-rima <touileb.karima@icloud.com>
2025-02-19 22:47:16 +0000

    Write a program that prints all the numbers of base 16 in lowercase, followed by a new line

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	/** declaration of variables */
+	int i;
+	int k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	for (k = 'a'; i >= 'f'; i++)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
+
