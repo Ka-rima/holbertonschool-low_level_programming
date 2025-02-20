c9fd0f7383fec894b3ae3199c4306e043717f730
Ka-rima <touileb.karima@icloud.com>
2025-02-20 21:16:14 +0000

    I added another fonction

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -15,14 +15,15 @@ int main(void)
 	for (k = 0; k <= 9; k++)
 	{
 		putchar(k);
-
-		if (k <= 9)
+	}
+	
+for (k = 0; k <= 9; k++)
 	{
 		putchar(',');
 		putchar(' ');
 	}
-}
+
 	putchar('\n');
-	return (0);
+	return(0);
 }
 
