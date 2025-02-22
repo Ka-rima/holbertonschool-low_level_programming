5e791fe3047eff1ed60a4821be16a127e68c85d5
Ka-rima <touileb.karima@icloud.com>
2025-02-22 21:32:53 +0000

    Remove the return, remove a space

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,21 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
 /**
  * print_alphabet - prints lowercase alphabet followed by a new line
+ *
+ * Return: 0 (Success)
  */
 
 void print_alphabet(void)
-
-#endif /* MAIN_H */
-
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		_putchar(i);
-	}
-		_putchar('\n');
+		_putchar(i); 
+	
+}
+	_putchar('\n');
 }
 
