4b0077aaabce4f8778040e757ffa2d1a7e385f8d
Ka-rima <touileb.karima@icloud.com>
2025-02-23 11:19:42 +0000

    Declaration of the function print_alphabet time 10

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);  /** Function for _putchar */
-void print_alphabet(void);  /** DÃ©claration de la fonction print_alphaet */
+void print_alphabet(void);  /** De©clarationof fonction print_alphabet */
+void print_alphabet_x10(void) /** Declaration of fonction print_alphabet_x10 */
 
 #endif /* MAIN_H */
 
