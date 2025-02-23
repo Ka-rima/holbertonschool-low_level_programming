d6ecbfac457f0ffc997149f821a4e2f8cff0a84b
Ka-rima <touileb.karima@icloud.com>
2025-02-23 11:57:19 +0000

    I added some description as per Betty style

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet 10 times in lower case
+ * print_alphabet_x10 - print_alphabet 10 times in lower case
+ *
+ * Return: 0 (success)
  */
 
 void print_alphabet_x10(void)
@@ -9,10 +11,8 @@ void print_alphabet_x10(void)
 	char i;
 	int a;
 
-	for (a = 0; a <= 10; a++)
+	for (a = 0; a < 10; a++)
 {
-	_putchar(i);
-}
 	for (i = 'a'; i <= 'z'; i++)
 {
 	_putchar(i);
