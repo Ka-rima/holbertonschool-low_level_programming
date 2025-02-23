90fb365893d3a4f383856789b70ad66e6b8e8c4b
Ka-rima <touileb.karima@icloud.com>
2025-02-23 13:22:20 +0000

    Betty style correction: removal of duplicate return, trailing whitespace, and added description for c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
-/** 
+/**
  * _islower - function that checks a character is in lowercase
  *
- * Return: 1 if c is lowercase
- * Return: 0 otherwise
+ * Return: 1 if c is lowercase 0 otherwise
  */
 
 int _islower(int c)
 {
+	int c;
+
 	if (c == 'a' && c <= 'z')
 		return (1);
 	else
