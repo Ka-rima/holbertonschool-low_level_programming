d15224d963ac6bb83aff7296922fa468b8b8bc81
Ka-rima <touileb.karima@icloud.com>
2025-02-23 14:12:17 +0000

    Declaration of function isalpha

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,6 +5,7 @@ int _putchar(char c);  /** Function for _putchar */
 void print_alphabet(void);  /** De©claratin of function print_alphabet */
 void print_alphabet_x10(void); /** Declaration of function print_alphabet_x10 */
 int _islower(int c); /** Function for _islower */
+int _isalpha(int c); /** Function for _iaslpha */
 
 #endif /* MAIN_H */
 
