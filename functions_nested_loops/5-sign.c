3d3eb28baad87f137abeb448613e49b3ca3813e9
Ka-rima <touileb.karima@icloud.com>
2025-02-23 16:41:51 +0000

    Correction of the comments to be conformed to Betty style

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * int print_sign - the function prints the sign of a number
+ * print_sign - the function prints the sign of a number
  *
  * @n: the variable we need to check
  *
- * Return: 1 and prints + if n is greater than zero, 0 and prints 0 if n is zero, -1 and prints - if n is less than zero
+ * Return: 1 and print + if n > zero, 0 and print 0 if n is zero
+ * -1 and print - if n is < zero
  */
 
 int print_sign(int n)
