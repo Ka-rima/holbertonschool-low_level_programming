a3efdda11e0fc95d8ee0a605fe4b7e235b17f520
Ka-rima <touileb.karima@icloud.com>
2025-02-23 11:47:35 +0000

    I added some extra curly braces

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,13 @@ void print_alphabet_x10(void)
 	int a;
 
 	for (a = 0; a <= 10; a++)
-		_putchar(i);
+{
+	_putchar(i);
+}
 	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
-
+{
+	_putchar(i);
+}
 	_putchar('\n);
 }
 
