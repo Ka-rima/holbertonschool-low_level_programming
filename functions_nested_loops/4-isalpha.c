cdef6c65ffecf18fde7163bcc19a621c2f012de4
Ka-rima <touileb.karima@icloud.com>
2025-02-23 14:26:58 +0000

    Write a function that checks for alphabetic character

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** _isalpha - Checks if the choosen character is a letter
+ *
+ * @c - the variable we will use for our function
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+	else
+	return(0);
+}
+
