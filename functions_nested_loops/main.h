57d0dc3b9530cebc097e59c82339d88e44ce45bd
Ka-rima <touileb.karima@icloud.com>
2025-02-23 11:20:39 +0000

    Correction of spelling mistakes

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);  /** Function for _putchar */
-void print_alphabet(void);  /** De©clarationof fonction print_alphabet */
-void print_alphabet_x10(void) /** Declaration of fonction print_alphabet_x10 */
+void print_alphabet(void);  /** De©claratio nof function print_alphabet */
+void print_alphabet_x10(void) /** Declaration of function print_alphabet_x10 */
 
 #endif /* MAIN_H */
 
