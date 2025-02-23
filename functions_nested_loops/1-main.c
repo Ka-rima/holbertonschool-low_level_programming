451f1f2524e4cecb6dcc768d70c0bddd5292da42
Ka-rima <touileb.karima@icloud.com>
2025-02-23 10:42:08 +0000

    I finish to write the code

--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
 /**
- * main - entry point
+ * main - check the code
  *
- * Return: void
+ * Return: Always 0.
  */
+int main(void)
+{
+       	print_alphabet();
+	return (0);
+}
 
