9e6ebc5362874e7b5267cf59cb39dab30f9bb7eb
Ka-rima <touileb.karima@icloud.com>
2025-02-26 10:00:02 +0000

    main.h

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);  /** Function for _putchar */
+void reset_to_98(int *n); /** function that takes a pointer to an int as parameter and updates the value it points to 98 */
+
+#endif /* MAIN_H */
+
