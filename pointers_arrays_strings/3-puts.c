ac6ceb33aefeaa2c61146dc33f727ae42f91db03
Ka-rima <touileb.karima@icloud.com>
2025-02-28 16:17:12 +0000

    Prints the string followed by a new line

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  _puts - function that prints a string, followed by a new line
+ *
+ * @str: parameter of funtion puts
+ *
+ */
+
+void _puts(char *str)
+{
+
+	while (*str != '\0') /** we stop at the end of the string */
+
+	_putchar(*str);
+	_putchar('\0');
+}
