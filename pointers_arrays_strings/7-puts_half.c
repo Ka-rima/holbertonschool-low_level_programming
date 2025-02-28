44bacc723f71f854dad06717fd509e2be0bcecff
Ka-rima <touileb.karima@icloud.com>
2025-02-28 22:48:34 +0000

    prints half of a string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,11 +12,14 @@ void puts_half(char *str)
 	int n = 0;
 	int half = n / 2;
 
+	while (str[n] != '\0')
+		n++;
+
 	while (str[half] != '\0')
 	{
+		_putchar(str[half]);
 		half++;
 	}
-
 	_putchar('\n');
 
 }
