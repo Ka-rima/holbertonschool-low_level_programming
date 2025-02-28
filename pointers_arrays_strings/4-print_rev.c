839a1bd230e12ab68fcb8711ca8af3ee6dec7366
Ka-rima <touileb.karima@icloud.com>
2025-02-28 19:16:58 +0000

    Adding putchar to print

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -14,11 +14,15 @@ void print_rev(char *s)
 	len = 0;
 
 	while (s[len] != '\0')
+	{
 		len++;
+	}
 
-	while (s[len] >= 0)
+	while (len >= 0)
+	{
 		_putchar(s[len]);
 		len--;
+	}
 
 	_putchar('\n');
 
