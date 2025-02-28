6368fba0304d8f699f7c8f0c84c5005013525350
Ka-rima <touileb.karima@icloud.com>
2025-02-28 19:37:05 +0000

    Reversing a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ *
+ * @s: variable
+ *
+ */
+
+void rev_string(char *s)
+{
+	char temp;
+	int start = 0;
+	int end = 0;
+
+	while (s[end] != '\0')
+	{
+		end++;
+	}
+		while (start < end)
+		{
+			temp = s[start];
+			s[start] = s[end];
+			s[end] = temp;
+
+			start++;
+			end--;
+		}
+}
