03943aff287873b85dcbb72098bb27b4883f124c
Ka-rima <touileb.karima@icloud.com>
2025-02-28 17:01:23 +0000

    Declaration of prototype print_rev

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ void reset_to_98(int *n); /** function that takes a pointer to an int as paramet
 void swap_int(int *a, int *b); /** function that swaps the values of two integers */
 int _strlen(char *s); /** function that returns the length of a string */
 void _puts(char *str); /** function that prints a string, followed by a new line, to stdout */
+void print_rev(char *s); /** function that prints a string, in reverse, followed by a new line */
 
 #endif /* MAIN_H */
 
