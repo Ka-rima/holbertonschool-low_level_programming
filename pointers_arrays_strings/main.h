9b99e71265a6e72d34f57e351844abab30a2f511
Ka-rima <touileb.karima@icloud.com>
2025-02-28 15:50:03 +0000

    Declaration of prototype void _puts(char *str)

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ int _putchar(char c);  /** Function for _putchar */
 void reset_to_98(int *n); /** function that takes a pointer to an int as parameter and updates the value it points to 98 */
 void swap_int(int *a, int *b); /** function that swaps the values of two integers */
 int _strlen(char *s); /** function that returns the length of a string */
+void _puts(char *str); /** function that prints a string, followed by a new line, to stdout */
 
 #endif /* MAIN_H */
 
