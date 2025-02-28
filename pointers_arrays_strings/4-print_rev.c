6f0fe96ac3a5bd0ceb769543ac5c5f9b558e9dcc
Ka-rima <touileb.karima@icloud.com>
2025-02-28 19:07:28 +0000

    Adding while loop

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,16 @@
 
 void print_rev(char *s)
 {
-	while (*s != '\0')
+	int len;
 
-	_putchar(*s);
-	s--;
+	len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (len >= 0)
+		len--;
+
 	_putchar('\n');
 
 }
