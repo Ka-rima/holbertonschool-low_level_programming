bd627bdabb1747698ae20a1631f3e13cea48372f
Ka-rima <touileb.karima@icloud.com>
2025-03-01 14:50:57 +0000

    Declaration of prototype _putchar _isupper

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);  /** Function for _putchar */
+int _isupper(int c); /** function that checks for uppercase character */
+
+#endif /* MAIN_H */
