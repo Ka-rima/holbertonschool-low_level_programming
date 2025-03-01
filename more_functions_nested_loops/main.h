a2045eea3ba6df9c3c5f732da7ec78524aa2130a
Ka-rima <touileb.karima@icloud.com>
2025-03-01 15:48:59 +0000

    Update prototype

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,6 +3,6 @@
 
 int _putchar(char c);  /** Function for _putchar */
 int _isupper(int c); /** function that checks for uppercase character */
-void more_numbers(void); /** function that prints 10 times the numbers, from 0 to 14 */
+int _isdigit(int c); /** function that checks for a digit */
 
 #endif /* MAIN_H */
