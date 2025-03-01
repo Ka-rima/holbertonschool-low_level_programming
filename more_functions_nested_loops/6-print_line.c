95188a1ea5b29eabcca1fec860b4b3619b35f7d4
Ka-rima <touileb.karima@icloud.com>
2025-03-01 21:32:11 +0000

    Modification of conditions

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -8,17 +8,19 @@
 
 void print_line(int n)
 {
+	int i;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 
 	else
+
 	{
-		if (n >= 0)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
-			n++;
 		}
 			_putchar('\n');
 		}
