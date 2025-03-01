82cd2af35a2f9d1a96eb96777ff19815ef2f5de3
Ka-rima <touileb.karima@icloud.com>
2025-03-01 15:51:11 +0000

    Declaration of prototype mul

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 int _putchar(char c);  /** Function for _putchar */
 int _isupper(int c); /** function that checks for uppercase character */
 int _isdigit(int c); /** function that checks for a digit */
+int mul(int a, int b); /** function that multiplies two integers */
 
 #endif /* MAIN_H */
