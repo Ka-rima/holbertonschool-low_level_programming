95d534a46f109d018f348b6f2e5758733040a0e2
Ka-rima <touileb.karima@icloud.com>
2025-03-01 16:01:10 +0000

    Declaration of prototype print_numbers

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -5,5 +5,5 @@ int _putchar(char c);  /** Function for _putchar */
 int _isupper(int c); /** function that checks for uppercase character */
 int _isdigit(int c); /** function that checks for a digit */
 int mul(int a, int b); /** function that multiplies two integers */
-
+void print_numbers(void); /**  function that prints the numbers */
 #endif /* MAIN_H */
