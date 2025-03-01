d3e50f84280ba9769b55c5a15f5ca7a5abc1b213
Ka-rima <touileb.karima@icloud.com>
2025-03-01 16:54:19 +0000

    Declaration of prototype more_numbers

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,5 +7,6 @@ int _isdigit(int c); /** function that checks for a digit */
 int mul(int a, int b); /** function that multiplies two integers */
 void print_numbers(void); /**  function that prints the numbers */
 void print_most_numbers(void); /** function that prints the numbers, from 0 to 9 **/
+void more_numbers(void); /** function that prints 10 times the numbers */
 
 #endif /* MAIN_H */
