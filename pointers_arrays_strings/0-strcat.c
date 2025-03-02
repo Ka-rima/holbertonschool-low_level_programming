f20509f3a0102ae73560d2429a53fb1f1af91f7d
Ka-rima <touileb.karima@.com>
2025-03-02 21:42:04 +0000

    Adding incrementation and the null terminator

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -12,17 +12,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-	if (*dest != '\0')
+	while (*dest != '\0')
 	{
 		dest++;
 	}
 
-	if (*src != '\0')
+	while (*src != '\0')
 	{
 		*dest = *src;
 		src++;
 		dest++;
 	}
+
 	*dest = '\0';
 
 	return (dest);
